e8598b5a1f99092ef1ac21c33206fc9165c3ceb7
arletteru <arletteruiz03@gmail.com>
2026-05-21 18:30:40 -0600

    refactor: configurate app in order to router

--- lib/core/navigation/nutritrack.dart
+++ lib/core/navigation/nutritrack.dart
@@ -1,47 +1,62 @@
 import 'package:flutter/material.dart';
-import 'package:nutritrack/features/assessment/presentation/pages/assessment.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:go_router/go_router.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class NutritrackApp extends StatefulWidget {
-  const NutritrackApp({super.key});
+class NutritrackApp extends HookConsumerWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const NutritrackApp({
+    super.key,
+    required this.navigationShell,
+  });
 
   @override
-  State<NutritrackApp> createState() => _NutritrackAppState();
+  Widget build(BuildContext context, WidgetRef ref) {
+
+      useEffect(() {
+        debugPrint(
+          'Current tab: ${navigationShell.currentIndex}',
+        );
+        return null;
+      }, [navigationShell.currentIndex]);
+
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Nutritrack'),
+        ),
+
+        body: navigationShell,
+
+        bottomNavigationBar: BottomNavigationBar(
+          showSelectedLabels: false,
+          showUnselectedLabels: false,
+
+          currentIndex: navigationShell.currentIndex,
+
+          onTap: (index) {
+            navigationShell.goBranch(index);
+          },
+
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home), 
+              label: 'Home'
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.assessment),
+              label: 'Assessment',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.settings),
+              label: 'Settings',
+            ),
+          ],
+        ),
+      );
+    }
+
+    PreferredSizeWidget buildAppBar() {
+      return AppBar(title: const Text('Nutritrack'));
+    }
 }
-
-class _NutritrackAppState extends State<NutritrackApp> {
-  int _currentIndex = 0;
-
-  final List<Widget> _pages = [
-    const Placeholder(), 
-    const AssessmentPage(),    
-    const Placeholder(), 
-  ];
-
-
-   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: _buildAppBar(),
-      body: _pages[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        currentIndex: _currentIndex,
-        onTap: (index) => setState(() => _currentIndex = index),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.assessment), label: 'Assessment'),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: 'Settings'),
-        ],
-      ),
-    );
-  }
-
-  PreferredSizeWidget _buildAppBar() {
-    return AppBar(
-      title: const Text('Nutritrack'),
-    );
-  }
-}
-
-
